a9ed95e4a3ad422b12667e9fb5bd1d3ff108527c
s <s@localhost.localdomain>
2023-07-24 19:33:25 +0700

    add logo

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -42,11 +42,7 @@ const Navbar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          <img
-            src={logo}
-            alt="logo"
-            className="w-9 h-9 object-contain bg-white"
-          />
+          <img src={logo} alt="logo" className="w-24 h-24 object-contain " />
           <p className="text-white text-[18px] font-bold cursor-pointer flex ">
             Sony &nbsp;
             <span className="sm:block hidden"> | Front End Developer</span>
